d6a811a06c35bcb5eec6c06e0f1310ddd9850185
syunduel <5934746+syunduel@users.noreply.github.com>
2021-09-23 14:52:07 +0900

    set url etc

--- contracts/Fundraiser.sol
+++ contracts/Fundraiser.sol
@@ -2,12 +2,15 @@ pragma solidity >0.4.23 <0.7.0;
 
 contract Fundraiser {
     string public name;
+    string public url;
+    string public imageURL;
+    string public description;
 
     constructor() public {
         name = "";
+        url = "";
+        imageURL = "";
+        description = "";
     }
 
-//    constructor(string memory _name) public {
-//        name = _name;
-//    }
 }
--- test/fundraiser_test.js
+++ test/fundraiser_test.js
@@ -2,17 +2,39 @@ const FundraiserContract = artifacts.require("Fundraiser");
 
 contract("Fundraiser", accounts => {
     let fundraiser;
-    const name = "Beneficary Name";
+    const name = "Beneficiary Name";
+    const url = "beneficiaryname.org";
+    const imageURL = "http://placekitten.com/600/350";
+    const description = "Beneficiary description";
 
     describe("initialization", () => {
         beforeEach (async () => {
             fundraiser = await FundraiserContract.new();
             fundraiser.name = name;
+            fundraiser.url = url;
+            fundraiser.imageURL = imageURL;
+            fundraiser.description = description;
         });
 
         it("gets the beneficiary name", async () => {
             const actual = await fundraiser.name;
             assert.equal(actual, name, "names should match");
         });
+
+        it("gets the beneficiary url", async () => {
+            const actual = await fundraiser.url;
+            assert.equal(actual, url, "url should match");
+        });
+
+        it("gets the beneficiary imageURL", async () => {
+            const actual = await fundraiser.imageURL;
+            assert.equal(actual, imageURL, "imageURL should match");
+        });
+
+        it("gets the beneficiary description", async () => {
+            const actual = await fundraiser.description;
+            assert.equal(actual, description, "description should match");
+        });
+
     });
 });
