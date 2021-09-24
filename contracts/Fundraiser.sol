812703d83b90091e10ad864025e01bbcdc842fca
syunduel <5934746+syunduel@users.noreply.github.com>
2021-09-24 09:12:37 +0900

    6.5まで完了

--- contracts/Fundraiser.sol
+++ contracts/Fundraiser.sol
@@ -2,12 +2,27 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract Fundraiser is Ownable {
+
+    event DonationReceived(address indexed donor, uint256 value);
+
+    using SafeMath for uint256;
+    uint256 public totalDonations;
+    uint256 public donationsCount;
+
+    struct Donation {
+        uint256 value;
+        uint256 date;
+    }
+    mapping(address => Donation[]) private _donations;
+
     string public name;
     string public url;
     string public imageURL;
     string public description;
+ 
     address payable public beneficiary;
 
     constructor(
@@ -28,6 +43,39 @@ contract Fundraiser is Ownable {
 
     function setBeneficiary(address payable _beneficiary) public onlyOwner {
         beneficiary = _beneficiary;
+    }
+
+    function donate() public payable {
+        Donation memory donation = Donation({
+            value: msg.value,
+            date: block.timestamp
+        });
+        _donations[msg.sender].push(donation);
+        totalDonations = totalDonations.add(msg.value);
+        donationsCount++;
+
+        emit DonationReceived(msg.sender, msg.value);
+    }
+
+    function myDonationsCount() public view returns(uint256) {
+        return _donations[msg.sender].length;
+    }
+
+    function myDonations() public view returns(
+        uint256[] memory values,
+        uint256[] memory dates
+    ) {
+        uint256 count = myDonationsCount();
+        values = new uint256[](count);
+        dates = new uint256[](count);
+
+        for (uint256 i = 0; i < count; i++) {
+            Donation storage donation = _donations[msg.sender][i];
+            values[i] = donation.value;
+            dates[i] = donation.date;
+        }
+
+        return (values, dates);
     }
 
 }
